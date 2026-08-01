9ba4a677f4abcacc5d044f9a1cac14aee06753f0
Ben Jacobson-Bell <bjacobell@gmail.com>
2026-08-01 09:18:38 +0100

    Added broken power laws and low-freq turnovers to spectral_index.py

--- src/spectral_index.py
+++ src/spectral_index.py
@@ -12,7 +12,9 @@ plt.rcParams['font.family'] = 'serif'
 plt.rcParams['mathtext.fontset'] = 'dejavuserif'
 
 def apply_spec_idx(data, freq,
-           spec_idx=-1.7, ref_freq=400):
+           spec_idx=-1.7, ref_freq=400,
+           lft=False, beta=1.8, fc=70,
+           bpl=False, spec_idx_2=-1.59, fb=1400):
     '''
     Given an input array `data` labeled by input vector `freq`, 
     apply a spectral index to modulate the amplitude of `data` over frequency.
@@ -21,11 +23,28 @@ def apply_spec_idx(data, freq,
     There is no assumption as to the other dimensions; 
     `data` can be 2D (freq, time) or 3D (freq, pol, time).
 
+    lft: Does the pulsar spectrum have a low-frequency turnover (LFT)? If so, set to True.
+    beta: Used only if LFT = True to set the smoothness of the turnover; see Jankowski+18.
+    fc: Used only if LFT = True to set the turnover frequency [MHz].
+
     By default, spec_idx=-1.7 and ref_freq=400 (MHz), as is appropriate for
     PSR J0630-2834 per Jankowski+18.
 
-    TODO: Simulate broken power laws and low-frequency turnovers.
+    TODO: Simulate log-parabolic spectra.
     '''
     gains = np.array([(f/ref_freq)**spec_idx for f in freq])
+
+    if lft == True:
+        turnover_gains = np.array([np.exp((spec_idx/beta) * (f/fc)**(-1*beta)) for f in freq])
+        gains *= turnover_gains
+    if bpl == True:
+        gains = []
+        for f in freq:
+            if f < fb:
+                gains.append((f/ref_freq)**spec_idx)
+            else:
+                gains.append(((f/ref_freq)**spec_idx_2) * (fb/ref_freq)**(spec_idx-spec_idx_2))
+        gains = np.array(gains)
+    
     data2 = data * gains[:,None]
     return data2
